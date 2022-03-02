19865ae753c0c9355b8126e15a611911d12871d6
Justin Ng <jng193@my.bcit.ca>
2022-03-01 21:57:22 -0800

    added cards and images

--- scripts/main.js
+++ scripts/main.js
@@ -35,3 +35,73 @@ function insertName() {
     });
 }
 insertName();
+
+function writeHikes() {
+    //define a variable for the collection you want to create in Firestore to populate data
+    var hikesRef = db.collection("hikes");
+
+    hikesRef.add({
+        code:"S01",
+        name: "Brandywine Falls",    //replace with your own city?
+        city: "Squamish",
+        province: "BC",
+        level: "easy",
+        length: "1 km",
+        details: "I haven't been here in a while, but it's one of my favourites.",
+        img: "Brandywine Falls"
+
+    });
+    hikesRef.add({
+        code:"AM01",
+        name: "Lynn Canyon",    //replace with your own city?
+        city: "Anmore",
+        province: "BC",
+        level: "moderate",
+        length: "10.5 km",
+        details: "I go here regularly. The suspension bridge is cool!",
+        img: "Lynn Canyon"
+
+
+    });
+    hikesRef.add({
+        code:"NV01",
+        name: "Grouse Mountain",    //replace with your own city?
+        city: "North Vancouver",
+        province: "BC",
+        level: "hard",
+        length: "8.2 km",
+        details: "The Grouse Grind here is intense, but makes for a good workout.",
+        img: "grouseMountain"
+    });
+}
+
+function displayCards(collection) {
+    let hikeCardTemplate = document.getElementById("hikeCardTemplate");
+
+    db.collection(collection).get()
+        .then(snap => {
+            var i = 1;
+            snap.forEach(doc => { //iterate thru each doc
+                var title = doc.data().name;   // get value of the "name" key
+                var details = doc.data().details;   // get value of the "details" key
+                var image = doc.data().img;
+                let newcard = hikeCardTemplate.content.cloneNode(true);
+
+                //update title and text and image
+                newcard.querySelector('.card-title').innerHTML = title;
+                newcard.querySelector('.card-text').innerHTML = details;
+                newcard.querySelector('.card-image').src = "./images/" + image + ".jpg"; //hikes.jpg
+
+                //give unique ids to all elements for future use
+                newcard.querySelector('.card-title').setAttribute("id", "ctitle" + i);
+                newcard.querySelector('.card-text').setAttribute("id", "ctext" + i);
+                newcard.querySelector('.card-image').setAttribute("id", "cimage" + i);
+
+                //attach to gallery
+                document.getElementById(collection + "-go-here").appendChild(newcard);
+                i++;
+            })
+        })
+}
+
+displayCards("hikes");
